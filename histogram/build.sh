8dac0cb719d758c0d10e255657f693661d8d49a8
Akilan <me@akilan.io>
2023-05-22 14:32:17 +0100

    added fix to build historgram

--- histogram/build.sh
+++ histogram/build.sh
@@ -1,7 +1,7 @@
 # $CC $CFLAGS -I/home/akilan/cheri/output/morello-sdk/lib/clang/13.0.0/include/ -lpthread test.c 
 # Working build code
 
-$1 --config cheribsd-morello-purecap.cfg -lpthread kmeans.c -o ../bin/kmeans-purecap.out
-$1 --config cheribsd-morello-aarch64-for-hybrid-rootfs.cfg -lpthread kmeans.c -o ../bin/kmeans-hybrid-rootfs.out
-$1 --config cheribsd-morello-aarch64-for-purecap-rootfs.cfg -lpthread kmeans.c -o ../bin/kmeans-purecap-rootfs.out
-$1 --config cheribsd-morello-purecap-for-hybrid-rootfs.cfg -lpthread kmeans.c -o ../bin/kmeans-hybrid-rootfs.out
+$1 --config cheribsd-morello-purecap.cfg -lpthread histogram-pthreads.c -o ../bin/histogram-pthreads-purecap.out
+$1 --config cheribsd-morello-aarch64-for-hybrid-rootfs.cfg -lpthread histogram-pthreads.c -o ../bin/histogram-pthreads-hybrid-rootfs.out
+$1 --config cheribsd-morello-aarch64-for-purecap-rootfs.cfg -lpthread histogram-pthreads.c -o ../bin/histogram-pthreads-purecap-rootfs.out
+$1 --config cheribsd-morello-purecap-for-hybrid-rootfs.cfg -lpthread histogram-pthreads.c -o ../bin/histogram-pthreads-hybrid-rootfs.out
